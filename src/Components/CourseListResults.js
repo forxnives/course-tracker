b66b3d5aa7e389593ab94d960489c104125e1f6f
forxnives <forxnives@gmail.com>
2021-04-17 21:15:02 -0700

    spinning up server and connecting db

--- src/Components/CourseListResults.js
+++ src/Components/CourseListResults.js
@@ -1,9 +1,50 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import CourseListFilters from './CourseListFilters.js'
 import CourseCard from './CourseCard.js'
 
 
 function CourseListResults() {
+
+
+	const [ courses, setCourses ] = useState([])
+	const [ error, setError ] = useState(null)
+
+	useEffect(()=>{
+        
+		const retrieveCourses = async (e) => {
+	
+			try {
+				console.log(window.location.origin)
+				const response = await fetch(`http://localhost:8082/courses`, {
+					method: 'GET',
+					headers: {
+					  'Content-Type': 'application/json',
+					},
+				
+				})
+				if (response.status===500) {
+					throw new Error('Failure to retrieve')
+				}
+	
+				const data = await response.json()
+				setCourses(data.data)
+			  
+			} catch(err) {
+				setError(err.message);
+			}
+		}
+
+		retrieveCourses()
+		console.log('thing work??')
+
+	},[])
+
+
+	console.log(courses)
+	
+	
+
+
     return (
 		<div class="container margin_60_35">
 			<div class="row">
